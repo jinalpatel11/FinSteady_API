7bd14a15b3c24537138faa15e01b036962878245
Jinal Patel <jpatel6625@conestogac.on.ca>
2024-05-16 18:19:03 -0400

    RepositoryExtensions added for all table

--- Repositories/RepositoryExtensions.cs
+++ Repositories/RepositoryExtensions.cs
@@ -32,6 +32,52 @@ namespace SmartSaver_backend.Repositories
 
         }
 
+        public static void Map(this SavingGoal dbSavingGoal, SavingGoal savingGoal)
+        {
+            dbSavingGoal.GoalId = savingGoal.GoalId;
+            dbSavingGoal.UserId = savingGoal.UserId;
+            dbSavingGoal.Name = savingGoal.Name;
+            dbSavingGoal.Image = savingGoal.Image;
+            dbSavingGoal.TargetAmount = savingGoal.TargetAmount;
+            dbSavingGoal.TargetDate = savingGoal.TargetDate;
+            dbSavingGoal.CategoryId = savingGoal.CategoryId;
+            dbSavingGoal.SavingRuleId = savingGoal.SavingRuleId;
+            dbSavingGoal.FundBalance = savingGoal.FundBalance;
+            dbSavingGoal.CreatedAt = savingGoal.CreatedAt;
+            dbSavingGoal.UpdatedAt = savingGoal.UpdatedAt;
+
+        }
+
+        public static void Map(this SavingRule dbSavingRule, SavingRule savingRule)
+        {
+            dbSavingRule.SavingRuleId = savingRule.SavingRuleId;
+            dbSavingRule.Rulename = savingRule.Rulename;
+            //   dbSavingRule.Description = savingRule.Description;
+            dbSavingRule.CreatedAt = savingRule.CreatedAt;
+            dbSavingRule.UpdatedAt = savingRule.UpdatedAt;
+
+        }
+        public static void Map(this Transaction dbTransaction, Transaction transaction)
+        {
+            dbTransaction.TransactionId = transaction.TransactionId;
+            dbTransaction.Date = transaction.Date;
+            dbTransaction.Amount = transaction.Amount;
+            dbTransaction.FromUserId = transaction.FromUserId;
+            dbTransaction.ToUserId = transaction.ToUserId;
+            dbTransaction.TransactionType = transaction.TransactionType;
+            dbTransaction.CreatedAt = transaction.CreatedAt;
+            dbTransaction.UpdatedAt = transaction.UpdatedAt;
+
+        }
+
+        public static void Map(this Category dbCategory, Category category)
+        {
+            dbCategory.CategoryId = category.CategoryId;
+            dbCategory.Name = category.Name;
+            dbCategory.CreatedAt = category.CreatedAt;
+            dbCategory.UpdatedAt = category.UpdatedAt;
+
+        }
     }
 
 }
